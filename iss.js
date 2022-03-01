64d26cc6890d233f44badfd211d9174deb1b0461
Harshbir Singh <singh.hveer1@gmail.com>
2022-03-01 05:06:30 -0700

    Implement function to fetch coordinates using IP

--- iss.js
+++ iss.js
@@ -27,6 +27,35 @@ const fetchMyIP = function(callback) {
     }
   });
 };
+ 
 
 
-module.exports = fetchMyIP;
+//Function to fetch coordinates using ip address
+const fetchCoordsByIP = function(ip, callback) {
+
+  request(`https://api.freegeoip.app/json/75.155.85.228?apikey=c6ad75c0-9950-11ec-980d-6357375b7b64`, (error, response, body) => {
+    
+    if (error) {
+      callback(error, null);
+      return;
+
+    } else if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+
+    } else {
+      let jsonObject = JSON.parse(body);
+
+      // Save just the longitude and latutude from jsonObject in a new object
+      let latLong = {latitude: jsonObject.latitude, longitude: jsonObject.longitude};
+    
+      callback(null, latLong);
+      return;
+    }
+  });
+};
+
+
+module.exports = {fetchMyIP, fetchCoordsByIP};
+
